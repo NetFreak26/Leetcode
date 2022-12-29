aef32a9d65fb4d2800ea131f9b527e974d4d23c0
NetFreak26 <53409747+NetFreak26@users.noreply.github.com>
2022-12-29 17:15:31 +0530

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ump;
+
+        for(int i=0; i<nums.size(); i++) {
+            int find = target - nums[i];
+            if(ump.find(find) != ump.end()) {
+                return {i, ump[find]};
+            }
+            ump[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
