dbc0ba3a21c27bc7d5168fd2079dc206f08f7fc8
Chirayu Garg <53409747+NetFreak26@users.noreply.github.com>
2024-05-17 09:49:58 -0700

    Create 15_3Sum.cpp
    LeetCode Problem 15

---
+++ 15_3Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>> ans;
+        int n = nums.size();
+        for (int i=0; i<n; i++) {
+            int j = i+1;
+            int k = n-1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        vector<vector<int>> unq(ans.begin(), ans.end());
+        return unq;
+    }
+};
